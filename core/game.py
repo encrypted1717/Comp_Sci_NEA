245a90ea88943e7f6476d65f5dfb0e1537975589
encrypted1717 <arthur.mcoelho17@gmail.com>
2025-12-20 13:28:45 +0000

    renamed any mention to windows to display and implemented sprite functions for managing collisions and entities

--- core/game.py
+++ core/game.py
@@ -2,25 +2,41 @@ import pygame
 from core.entity import Entity
 from core.config_manager import ConfigManager
 from core.button import Button
+from core.collider import Collider
+from core.collision_manager import CollisionManager
 
 
 class Game(Button):
-    def __init__(self, window):
+    # noinspection PyTypeChecker
+    def __init__(self, display):
         super().__init__()
         # Main Setup
-        self.window = window
+        self.display = display
         self.dt = 0 # Seconds
         self.events = None
-        # Player setup
-        self.player = Entity((680, 540))
         # Setup configurations
         self.config_manager = ConfigManager()
         self.config = self.config_manager.get_config()
         self.config_manager.open_file("assets\\game_settings\\config_user.ini")
+        # Player setup
+        self.player = Entity((680, 450))
+        # Setup static objects
+        self.ground = Collider(pygame.Rect(0, 1080, 1920, 100), "ground")
+        # Setup collisions
+        self.colliders = pygame.sprite.Group()
+        self.colliders.add(self.ground)
+
+        self.entities = pygame.sprite.Group()
+        self.entities.add(self.player)
+
+        self.collision_manager = CollisionManager(self.entities, self.colliders)
+
+        # Map setup
 
     def event_handler(self, events):
         self.events = events
-        self.player.event_handler(self.events)
+        for entity in self.entities:
+            entity.event_handler(self.events)
         for event in self.events:
             #kb press down
             if event.type == pygame.KEYDOWN:
@@ -35,6 +51,9 @@ class Game(Button):
 
     def draw(self, dt):
         self.dt = dt
-        self.player.update(self.dt)
-        self.player.draw(self.window)
-        # Needed to avoid truncation error
+        self.colliders.draw(self.display)
+        self.entities.update(self.dt)
+        self.collision_manager.resolve_entity()
+        self.entities.draw(self.display)
+        for entity in self.entities:
+            pygame.draw.rect(self.display,(255, 0, 0), entity.rect, 5)
