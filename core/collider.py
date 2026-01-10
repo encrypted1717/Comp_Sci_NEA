741ffc514881fd46dc1909278ac1f56c7f0b5180
encrypted1717 <arthur.mcoelho17@gmail.com>
2026-01-10 00:47:14 +0000

    color brown

--- core/collider.py
+++ core/collider.py
@@ -5,6 +5,6 @@ class Collider(pygame.sprite.Sprite):
     def __init__(self, rect: pygame.Rect, collider: str) -> None:
         super().__init__()
         self.image = pygame.Surface(rect.size)
-        self.image.fill(pygame.Color('yellow'))
+        self.image.fill(pygame.Color('brown'))
         self.rect = self.image.get_rect(bottom=rect.top)
         self.collider_type = collider
