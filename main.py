90b921190a5f9351bd4acb67e7fec0d7a2a177f8
encrypted1717 <arthur.mcoelho17@gmail.com>
2026-01-18 15:03:34 +0000

    added a get_display function and code to update display

--- main.py
+++ main.py
@@ -4,27 +4,48 @@ import logging
 from core.window_manager import WindowManager
 from core.config_manager import ConfigManager
 
+
 # TODO fix logic as it is a bit off as after checking if files couldn't be open it doesn't make sense to attempt to open the path again
 def get_settings(path, logger):
-    config_manager = ConfigManager()
-    if config_manager.open_file(path):
+    config_manager = ConfigManager(path)
+    if config_manager.file_read:
         config = config_manager.get_config()
         width = config.getint("Graphics", "Screen_Width")
         height = config.getint("Graphics", "Screen_Height")
         display_mode = config.get("Window", "Display_Mode")
         fps = config.getint("Window", "FPS")
+    # TODO get default values not python set values through the programming
     else:
         logger.warning("Could not open config file in path: %s", path)
         width, height = pygame.display.get_desktop_sizes()[0]
         display_mode = "Fullscreen"
         fps = 60
-        config_manager.set_value({
+        config_manager.set_values({
             "Graphics": {"Screen_Width": width, "Screen_Height": height},
             "Window": {"Display_Mode": display_mode, "FPS": fps}
         })
-        config_manager.open_file(path)
-        config = config_manager.get_config()
+        #config = config_manager.get_config()
     return width, height, display_mode, fps
+
+def get_display(width, height, mode, logger):
+    if mode == "Windowed":
+        # Width and height reduced to show top bar of window
+        width -= 150
+        height -= 200
+        flags = pygame.RESIZABLE
+    elif mode == "Borderless_Windowed":
+        flags = pygame.NOFRAME
+    elif mode in ("Fullscreen", "Default"):
+        flags = pygame.FULLSCREEN
+    else:
+        logger.error("Unknown display mode: %s", mode)
+        raise ValueError(f"Unknown mode: {mode}")
+
+    pygame.display.set_caption("Game")
+    display = pygame.display.set_mode((width, height), flags)
+    logger.info("Display setup with resolution: %s x %s", width, height)
+    logger.info("Display mode: %s", mode)
+    return display
 
 def main():
     logging.basicConfig(
@@ -39,23 +60,11 @@ def main():
     clock = pygame.time.Clock() # Controls tick speed
     prev_time = time.time()
 
-    settings_path = "assets\\game_settings\\config_user.ini"
-    display_width, display_height, display_mode, fps = get_settings(settings_path, log) #check if user settings are already created
+    user_settings_path = "assets\\game_settings\\config_user.ini"
+    display_width, display_height, display_mode, fps = get_settings(user_settings_path, log) #check if user settings are already created
     log.info("Settings have been loaded")
 
-    if display_mode == "Windowed":
-        flags = pygame.RESIZABLE
-    elif display_mode == "Borderless_Windowed":
-        flags = pygame.NOFRAME
-    elif display_mode in ("Fullscreen", "Default"):
-        flags = pygame.FULLSCREEN
-    else:
-        log.error("Unknown display mode: %s", display_mode)
-        raise ValueError(f"Unknown mode: {display_mode}")
-
-    display = pygame.display.set_mode((display_width, display_height), flags)
-    log.info("Display setup with resolution: %s x %s", display_width, display_height)
-    log.info("Display mode: %s", display_mode)
+    display = get_display(display_width, display_height, display_mode, log)
     manager = WindowManager(display)  # WindowManager handles everything to do with the windows (i.e. switching/creating/deleting windows)
 
     running = True
@@ -73,7 +82,15 @@ def main():
                 running = False
 
         display.fill((255, 255, 255)) # Clear screen with white background
-        manager.update_window(events)
+
+        # Update window and check whether a change in settings has taken place
+        state = manager.update_window(events)
+        if state == "update_display":
+            display_width, display_height, display_mode, fps = get_settings(user_settings_path, log)
+            log.info("Settings have been reloaded")
+            display = get_display(display_width, display_height, display_mode, log)
+            manager.update_display(display)
+
         manager.draw(dt)
         pygame.display.flip()
     pygame.quit()
