858d53cb532a57abc951c13997d20733c608c834
encrypted1717 <arthur.mcoelho17@gmail.com>
2026-01-18 15:04:28 +0000

    added resolution scaler functionality and inherits from window now

--- menus/exit_menu.py
+++ menus/exit_menu.py
@@ -1,40 +1,38 @@
 import pygame
+from core.window import Window
 from core.button import Button
 
 
-class ExitMenu:
+class ExitMenu(Window):
     def __init__(self, display):
-        self.display = display
-        self.dt = None
-        self.events = None
-        self.font = pygame.font.Font("assets\\fonts\\OldeTome\\OldeTome.ttf", 43)
-        self.buttons = pygame.sprite.Group()
+        super().__init__(display)
         # noinspection PyTypeChecker
         self.buttons.add(
             Button(
-                (960, 540),
-                (550, 120),
+                (self.center_x, self.center_y),
+                (self.rs.x(550), self.rs.y(120)),
                 "Are you sure you want to exit",
-                self.font, "#ffffff",
+                pygame.font.Font(self.fonts["OldeTome"], self.rs.u(43)),
+                "#ffffff",
                 '#000000',
-                5,
+                self.rs.u(5),
                 border_colour = "#ffffff",
-                offset_y = 4,
+                offset_y = self.rs.y(4),
                 action = "exit",
                 hover_text_colour = "#000000",
                 hover_rect_colour = "#ffffff",
                 hover_border_colour = "#000000"
             ),
             Button(
-                (150, 120),
-                (175, 70),
+                (self.rs.x(150), self.rs.y(120)),
+                (self.rs.x(175), self.rs.y(70)),
                 "Back",
-                self.font,
+                pygame.font.Font(self.fonts["OldeTome"], self.rs.u(43)),
                 "#ffffff",
                 "#000000",
-                5,
+                self.rs.u(5),
                 border_colour = "#ffffff",
-                offset_y = 4,
+                offset_y = self.rs.y(4),
                 action = "main_menu",
                 hover_text_colour = "#000000",
                 hover_rect_colour = "#ffffff",
