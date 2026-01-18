081ee9c3350cdc28bb291e3c8a3a811504e48789
encrypted1717 <arthur.mcoelho17@gmail.com>
2026-01-18 15:05:27 +0000

    updated so each ConfigManager instance holds one path at a time

--- core/config_manager.py
+++ core/config_manager.py
@@ -2,14 +2,14 @@ from configparser import ConfigParser
 
 
 class ConfigManager:
-    def __init__(self):
+    def __init__(self, path: str):
+        self.__path = path
         self.__config = ConfigParser()
         self.__config.optionxform = str  # this line just makes it so any writing I do to an ini file is saved the way I type it
-        self.__path = None
+        self.file_read = self.__load()
 
-    def open_file(self, path: str) -> bool:
+    def __load(self) -> bool:
         self.__config.clear()
-        self.__path = path
         # Returns if file exists or isn't corrupt
         return bool(self.__config.read(self.__path))
 
@@ -17,11 +17,14 @@ class ConfigManager:
         return self.__config
 
     # Allows you set many values instead of just one
-    def set_value(self, update: dict) -> None:
+    def set_values(self, update: dict) -> None:
         for section, option_value in update.items():
             if not self.__config.has_section(section):
                 self.__config.add_section(section)
             for option, value in option_value.items():
                 self.__config.set(section, option, str(value))
+        self.__save()
+
+    def __save(self):
         with open(self.__path, "w") as save:
             self.__config.write(save)
