2e302848d4231c772f54b45124b1e7d0341edb19
encrypted1717 <arthur.mcoelho17@gmail.com>
2026-01-21 11:17:02 +0000

    accepts renderer as a param and updated slightly

--- menus/exit_menu.py
+++ menus/exit_menu.py
@@ -4,35 +4,34 @@ from core.button import Button
 
 
 class ExitMenu(Window):
-    def __init__(self, display):
-        super().__init__(display)
-        # noinspection PyTypeChecker
+    def __init__(self, display, renderer):
+        super().__init__(display, renderer)
         self.buttons.add(
             Button(
                 (self.center_x, self.center_y),
-                (self.rs.x(550), self.rs.y(120)),
+                (550, 120),
                 "Are you sure you want to exit",
-                pygame.font.Font(self.fonts["OldeTome"], self.rs.u(43)),
+                pygame.font.Font(self.fonts["OldeTome"], 43),
                 "#ffffff",
                 '#000000',
-                self.rs.u(5),
+                5,
                 border_colour = "#ffffff",
-                offset_y = self.rs.y(4),
+                offset_y = 4,
                 action = "exit",
                 hover_text_colour = "#000000",
                 hover_rect_colour = "#ffffff",
                 hover_border_colour = "#000000"
             ),
             Button(
-                (self.rs.x(150), self.rs.y(120)),
-                (self.rs.x(175), self.rs.y(70)),
+                (150, 120),
+                (175, 70),
                 "Back",
-                pygame.font.Font(self.fonts["OldeTome"], self.rs.u(43)),
+                pygame.font.Font(self.fonts["OldeTome"], 43),
                 "#ffffff",
                 "#000000",
-                self.rs.u(5),
+                5,
                 border_colour = "#ffffff",
-                offset_y = self.rs.y(4),
+                offset_y = 4,
                 action = "main_menu",
                 hover_text_colour = "#000000",
                 hover_rect_colour = "#ffffff",
@@ -53,7 +52,5 @@ class ExitMenu(Window):
         return None
 
     def draw(self, dt):
-        self.dt = dt
-        mouse_pos = pygame.mouse.get_pos()
-        self.buttons.update(mouse_pos)
-        self.buttons.draw(self.display)
+        self.surface.fill((255, 255, 255))
+        super().draw(dt)
