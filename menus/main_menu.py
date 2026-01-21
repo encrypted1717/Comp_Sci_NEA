e638407795debe7e02bac3e457d2e66a5bab9860
encrypted1717 <arthur.mcoelho17@gmail.com>
2026-01-21 11:19:40 +0000

    updated for new renderer

--- menus/main_menu.py
+++ menus/main_menu.py
@@ -5,28 +5,26 @@ from graphics.parallax_background import Background
 
 
 class MainMenu(Window):
-    def __init__(self, display):
-        super().__init__(display)
+    def __init__(self, display, renderer):
+        super().__init__(display, renderer)
         # Setup parallax background
         self.bg = Background()
-        self.bg.resize((self.display_width, self.display_height)) # Can be used once again when resolution changes
+        self.bg.resize((self.display_width, self.display_height)) # Resize to virtual resolution (1920 x 1080)
         # Setup buttons
-        self.buttons = pygame.sprite.Group()
-        self.button_width = self.rs.x(255)
-        self.button_height = self.rs.y(70)
-        # noinspection PyTypeChecker
+        self.button_width = 255
+        self.button_height = 70
         self.buttons.add(
             Button(
-                (self.center_x, self.rs.y(500)),
+                (self.center_x, 500),
                 (self.button_width, self.button_height),
                 "Start",
-                pygame.font.Font(self.fonts["OldeTome"], self.rs.u(43)),
+                pygame.font.Font(self.fonts["OldeTome"], 43),
                 "#ffffff",
                 "#000000",
-                self.rs.u(5),
+                5,
                 border_colour = "#ffffff",
                 fill = False,
-                offset_y = self.rs.y(4),
+                offset_y = 4,
                 action = "start",
                 hover_text_colour = "#000000",
                 hover_rect_colour = "#ffffff",
@@ -34,16 +32,16 @@ class MainMenu(Window):
                 fill_on_hover = True
             ),
             Button(
-                (self.center_x, self.rs.y(590)),
+                (self.center_x, 590),
                 (self.button_width, self.button_height),
                 "Controls",
-                pygame.font.Font(self.fonts["OldeTome"], self.rs.u(43)),
+                pygame.font.Font(self.fonts["OldeTome"], 43),
                 "#ffffff",
                 "#000000",
-                self.rs.u(5),
+                5,
                 border_colour = "#ffffff",
                 fill = False,
-                offset_y = self.rs.y(4),
+                offset_y = 4,
                 action = "controls",
                 hover_text_colour = "#000000",
                 hover_rect_colour = "#ffffff",
@@ -51,16 +49,16 @@ class MainMenu(Window):
                 fill_on_hover = True
             ),
             Button(
-                (self.center_x, self.rs.y(680)),
+                (self.center_x, 680),
                 (self.button_width, self.button_height),
                 "Settings",
-                pygame.font.Font(self.fonts["OldeTome"], self.rs.u(43)),
+                pygame.font.Font(self.fonts["OldeTome"], 43),
                 "#ffffff",
                 "#000000",
-                self.rs.u(5),
+                5,
                 border_colour = "#ffffff",
                 fill = False,
-                offset_y = self.rs.y(4),
+                offset_y = 4,
                 action = "settings",
                 hover_text_colour = "#000000",
                 hover_rect_colour = "#ffffff",
@@ -68,16 +66,16 @@ class MainMenu(Window):
                 fill_on_hover = True
             ),
             Button(
-                (self.center_x, self.rs.y(770)),
+                (self.center_x, 770),
                 (self.button_width, self.button_height),
                 "Exit",
-                pygame.font.Font(self.fonts["OldeTome"], self.rs.y(43)),
+                pygame.font.Font(self.fonts["OldeTome"], 43),
                 "#ffffff",
                 "#000000",
-                self.rs.u(5),
+                5,
                 border_colour = "#ffffff",
                 fill = False,
-                offset_y = self.rs.u(4),
+                offset_y = 4,
                 action = "exit",
                 hover_text_colour = "#000000",
                 hover_rect_colour = "#ffffff",
@@ -96,5 +94,5 @@ class MainMenu(Window):
 
     def draw(self, dt):
         self.bg.update(dt)
-        self.bg.draw(self.display)
+        self.bg.draw(self.surface) # Draw to virtual surface
         super().draw(dt)
