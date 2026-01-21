68679317a286b2e7d7a4eec63c5008661afaaa7b
encrypted1717 <arthur.mcoelho17@gmail.com>
2026-01-21 11:21:54 +0000

    updated for renderer includes processing mouse updates and updating with new displays/surfaces

--- core/window.py
+++ core/window.py
@@ -1,14 +1,18 @@
 import pygame
-from graphics.resolution_scaler import ResolutionScaler
 
 
 class Window:
-    def __init__(self, display):
+    def __init__(self, display, renderer):
         self.display = display
-        self.display_width, self.display_height = self.display.get_size()
-        self.rs = ResolutionScaler((self.display_width, self.display_height), (1920, 1080))
-        self.center_x = self.display_width / 2
-        self.center_y = self.display_height / 2
+        self.renderer = renderer
+
+        self.surface = self.renderer.virtual_surface
+
+        self.display_width, self.display_height = self.renderer.design_size # In this case its 1920, 1080
+
+        self.center_x = 960
+        self.center_y = 540
+
         self.events = None
         self.dt = 0
         self.fonts = {
@@ -19,13 +23,13 @@ class Window:
 
     def draw(self, dt):
         self.dt = dt
-        self.buttons.update(pygame.mouse.get_pos())
-        self.buttons.draw(self.display)  # keep loading buttons
+        mouse_pos = self.renderer.get_virtual_mouse_pos()
+        self.buttons.update(mouse_pos)
+        self.buttons.draw(self.surface)  # keep loading buttons
 
     # Update all variables with new display
     def set_display(self, new_display):
+        # Called when the window surface changes (resolution/mode/resizing)
         self.display = new_display
-        self.display_width, self.display_height = self.display.get_size()
-        self.rs = ResolutionScaler((self.display_width, self.display_height), (1920, 1080))
-        self.center_x = self.display_width / 2
-        self.center_y = self.display_height / 2
+        self.renderer.set_window_surface(new_display)
+        self.surface = self.renderer.virtual_surface
