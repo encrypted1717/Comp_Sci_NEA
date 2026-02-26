1e922568605f05b657b3f7ee300596c6470b00ac
encrypted1717 <arthur.mcoelho17@gmail.com>
2026-02-26 22:38:53 +0000

    Slight improvement

--- core/entity.py
+++ core/entity.py
@@ -3,7 +3,9 @@ import pygame
 from graphics.animation_manager import AnimationManager
 
 
-# TODO add more animations and support different times of punches/combos using delays/elapsed time between clicks
+# TODO add more animations
+# TODO support different times of punches/combos using delays/elapsed time between clicks
+# TODO fix bug: sprinting anim continues after releasing movement key before removing sprint key
 class Entity(pygame.sprite.Sprite):
     def __init__(self, start_position: tuple, sprite_type: str, health=100):
         super().__init__()
@@ -32,8 +34,7 @@ class Entity(pygame.sprite.Sprite):
         self.rect = self.body
         self.image = pygame.Surface(self.frame_size, pygame.SRCALPHA)
         self.img_rect = self.image.get_rect(midbottom=self.body.midbottom)
-        self.sprite_bounds = self.image.get_bounding_rect().move(
-            self.img_rect.topleft)  # Dynamic Hitbox based on animation
+        self.sprite_bounds = self.image.get_bounding_rect().move(self.img_rect.topleft)  # Dynamic Hitbox based on animation
 
         # Physics
         self.velocity = self.vector(0, 0)
@@ -74,15 +75,15 @@ class Entity(pygame.sprite.Sprite):
         self.animation_priority = {
             "default": 0,
             "walk": 0,
-            "sprint": 0,
-            "stop_sprint": 0,
-            "jump": 1,
-            "double_jump": 1,
-            "crouch": 2,
-            "charging": 2,
-            "punch_1": 2,
-            "jump_strike": 2,
-            "death": 2,
+            "sprint": 1,
+            "stop_sprint": 2,
+            "jump": 2,
+            "double_jump": 2,
+            "crouch": 3,
+            "charging": 3,
+            "punch_1": 3,
+            "jump_strike": 3,
+            "death": 4,
         }
         self.animation_manager.set_animation("default")
         self.__log.info("Entity created: id = %s sprite_type = %s", id(self), sprite_type)
@@ -116,21 +117,21 @@ class Entity(pygame.sprite.Sprite):
 
     def _load_animations(self) -> None:
         load = self.animation_manager.load_animation
-        base_fight = "assets\\characters\\default\\fighting\\Animations\\"
-        base_move = "assets\\characters\\default\\movement\\Animations\\"
-        base_other = "assets\\characters\\default\\other\\Animations\\"
+        address_fight = "assets\\characters\\default\\fighting\\Animations\\"
+        address_move = "assets\\characters\\default\\movement\\Animations\\"
+        address_other = "assets\\characters\\default\\other\\Animations\\"
 
-        load("default", base_fight + "punch_1.png", scale=self.sprite_scale, frame_indices=[0])
-        load("punch_1", base_fight + "punch_1.png", scale=self.sprite_scale, cooldown=0.035)
-        load("charging", base_fight + "skill_charging.png", scale=self.sprite_scale, cooldown=0.12)  # Make this a skill activation not a charging anim
-        load("jump", base_move + "upward_jump.png", scale=self.sprite_scale, frame_indices=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 3, 2, 1, 0], cooldown=0.04)
-        load("double_jump", base_move + "double_jump.png", scale=self.sprite_scale, cooldown=0.04)
-        load("walk", base_move + "walking.png", scale=self.sprite_scale)
-        load("sprint", base_move + "running.png", scale=self.sprite_scale)
-        load("stop_sprint", base_move + "stop_running.png", scale=self.sprite_scale)
-        load("crouch", base_move + "crouch.png", scale=self.sprite_scale, cooldown=0.035)
-        load("jump_strike", base_move + "jump_strike.png", scale=self.sprite_scale)
-        load("death", base_other + "death.png", scale=self.sprite_scale)
+        load("default", address_fight + "punch_1.png", scale=self.sprite_scale, frame_indices=[0])
+        load("punch_1", address_fight + "punch_1.png", scale=self.sprite_scale, cooldown=0.035)
+        load("charging", address_fight + "skill_charging.png", scale=self.sprite_scale, cooldown=0.12)  # Make this a skill activation not a charging anim
+        load("jump", address_move + "upward_jump.png", scale=self.sprite_scale, frame_indices=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 3, 2, 1, 0], cooldown=0.04)
+        load("double_jump", address_move + "double_jump.png", scale=self.sprite_scale, cooldown=0.04)
+        load("walk", address_move + "walking.png", scale=self.sprite_scale)
+        load("sprint", address_move + "running.png", scale=self.sprite_scale)
+        load("stop_sprint", address_move + "stop_running.png", scale=self.sprite_scale)
+        load("crouch", address_move + "crouch.png", scale=self.sprite_scale, cooldown=0.035)
+        load("jump_strike", address_move + "jump_strike.png", scale=self.sprite_scale)
+        load("death", address_other + "death.png", scale=self.sprite_scale)
 
     def update(self, dt):
         self.dt = dt
@@ -233,7 +234,7 @@ class Entity(pygame.sprite.Sprite):
             return
 
         self.flip_x = (axis < 0)
-        accel = self.horizontal_acceleration + (self.sprint_force if inp["sprint"] else 0.0)
+        accel = self.horizontal_acceleration + (self.sprint_force if (inp["sprint"] and not inp["down"]) else 0.0)
         self.acceleration.x = axis * accel
 
     def select_animation(self, inp):
@@ -279,7 +280,7 @@ class Entity(pygame.sprite.Sprite):
             return
 
         if self.animation_manager.is_playing():
-            if self.animation_priority.get(requested) <= self.animation_priority.get(current):
+            if self.animation_priority.get(requested) < self.animation_priority.get(current):
                 return
 
         self.animation_manager.set_animation(requested, loop=loop, restart=restart)
